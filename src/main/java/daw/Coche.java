91ac602cb1a01a7903d54025e6b75dc48b1d08cd
dnavbor167 <dnavbor167@g.educaand.es>
2024-01-15 09:46:04 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/daw/Coche.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author daniel
+ */
+public class Coche {
+    
+    //Creaión de atributos encapsulados
+    private int numRuedas;
+    private float cv;
+    private String matricula;
+}
