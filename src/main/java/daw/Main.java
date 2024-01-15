2eade5cfb3d2c19e9548cf34ec354b68bf67e8a5
jrommir834e <jrommir834e@g.educaand.es>
2024-01-15 10:04:04 +0100

    Creación Main y objetos.

---
+++ src/main/java/daw/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author jesus
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        Coche c1 =new Coche();
+        Coche c2=new Coche(4,1200,"50CENT");
+        
+        
+        System.out.println("Coche numero 1: "+c1);
+        System.out.println("COche numero 2: " + c2);
+    }
+}
